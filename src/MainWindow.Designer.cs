43254486b5e8417dc475650348459098744444ab
TorniX0 <43686655+TorniX0@users.noreply.github.com>
2021-11-29 18:50:39 +0200

    Add files via upload

---
+++ src/MainWindow.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace OpenTimerResolution
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.startButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.currentResolutionLabel = new System.Windows.Forms.Label();
+            this.minimumResolutionLabel = new System.Windows.Forms.Label();
+            this.maximumResolutionLabel = new System.Windows.Forms.Label();
+            this.textChanger = new System.Windows.Forms.Timer(this.components);
+            this.wantedResolutionLabel = new System.Windows.Forms.Label();
+            this.timerResolutionBox = new System.Windows.Forms.TextBox();
+            this.warningLabel = new System.Windows.Forms.Label();
+            this.intervalComboBox = new System.Windows.Forms.ComboBox();
+            this.textIntervalLabel = new System.Windows.Forms.Label();
+            this.minimizeIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.minimizeIconContextMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.minimizeIconContextMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(62, 159);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Enabled = false;
+            this.stopButton.Location = new System.Drawing.Point(178, 159);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(75, 23);
+            this.stopButton.TabIndex = 1;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // currentResolutionLabel
+            // 
+            this.currentResolutionLabel.AutoSize = true;
+            this.currentResolutionLabel.BackColor = System.Drawing.Color.Transparent;
+            this.currentResolutionLabel.Location = new System.Drawing.Point(62, 9);
+            this.currentResolutionLabel.Name = "currentResolutionLabel";
+            this.currentResolutionLabel.Size = new System.Drawing.Size(109, 15);
+            this.currentResolutionLabel.TabIndex = 2;
+            this.currentResolutionLabel.Text = "Current Resolution:";
+            // 
+            // minimumResolutionLabel
+            // 
+            this.minimumResolutionLabel.AutoSize = true;
+            this.minimumResolutionLabel.BackColor = System.Drawing.Color.Transparent;
+            this.minimumResolutionLabel.Location = new System.Drawing.Point(62, 37);
+            this.minimumResolutionLabel.Name = "minimumResolutionLabel";
+            this.minimumResolutionLabel.Size = new System.Drawing.Size(122, 15);
+            this.minimumResolutionLabel.TabIndex = 3;
+            this.minimumResolutionLabel.Text = "Minimum Resolution:";
+            // 
+            // maximumResolutionLabel
+            // 
+            this.maximumResolutionLabel.AutoSize = true;
+            this.maximumResolutionLabel.BackColor = System.Drawing.Color.Transparent;
+            this.maximumResolutionLabel.Location = new System.Drawing.Point(62, 66);
+            this.maximumResolutionLabel.Name = "maximumResolutionLabel";
+            this.maximumResolutionLabel.Size = new System.Drawing.Size(124, 15);
+            this.maximumResolutionLabel.TabIndex = 4;
+            this.maximumResolutionLabel.Text = "Maximum Resolution:";
+            // 
+            // textChanger
+            // 
+            this.textChanger.Enabled = true;
+            this.textChanger.Interval = 1000;
+            this.textChanger.Tick += new System.EventHandler(this.textChanger_Tick);
+            // 
+            // wantedResolutionLabel
+            // 
+            this.wantedResolutionLabel.AutoSize = true;
+            this.wantedResolutionLabel.BackColor = System.Drawing.Color.Transparent;
+            this.wantedResolutionLabel.Location = new System.Drawing.Point(68, 95);
+            this.wantedResolutionLabel.Name = "wantedResolutionLabel";
+            this.wantedResolutionLabel.Size = new System.Drawing.Size(110, 15);
+            this.wantedResolutionLabel.TabIndex = 5;
+            this.wantedResolutionLabel.Text = "Wanted Resolution:";
+            // 
+            // timerResolutionBox
+            // 
+            this.timerResolutionBox.Location = new System.Drawing.Point(185, 92);
+            this.timerResolutionBox.Name = "timerResolutionBox";
+            this.timerResolutionBox.ShortcutsEnabled = false;
+            this.timerResolutionBox.Size = new System.Drawing.Size(60, 23);
+            this.timerResolutionBox.TabIndex = 6;
+            this.timerResolutionBox.TextChanged += new System.EventHandler(this.timerResolutionBox_TextChanged);
+            this.timerResolutionBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.timerResolutionBox_KeyPress);
+            // 
+            // warningLabel
+            // 
+            this.warningLabel.AutoSize = true;
+            this.warningLabel.BackColor = System.Drawing.Color.Transparent;
+            this.warningLabel.ForeColor = System.Drawing.Color.Red;
+            this.warningLabel.Location = new System.Drawing.Point(23, 124);
+            this.warningLabel.Name = "warningLabel";
+            this.warningLabel.Size = new System.Drawing.Size(286, 30);
+            this.warningLabel.TabIndex = 7;
+            this.warningLabel.Text = "WARNING! YOU SHOULDN\'T DO THIS VALUE!\r\nDO 0.50 IF YOU DON\'T KNOW WHAT YOU\'RE DOIN" +
+    "G!";
+            this.warningLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.warningLabel.Visible = false;
+            // 
+            // intervalComboBox
+            // 
+            this.intervalComboBox.FormattingEnabled = true;
+            this.intervalComboBox.Items.AddRange(new object[] {
+            "1000ms",
+            "500ms",
+            "125ms",
+            "50ms"});
+            this.intervalComboBox.Location = new System.Drawing.Point(164, 202);
+            this.intervalComboBox.Name = "intervalComboBox";
+            this.intervalComboBox.Size = new System.Drawing.Size(121, 23);
+            this.intervalComboBox.TabIndex = 8;
+            this.intervalComboBox.SelectedIndexChanged += new System.EventHandler(this.intervalComboBox_SelectedIndexChanged);
+            // 
+            // textIntervalLabel
+            // 
+            this.textIntervalLabel.AutoSize = true;
+            this.textIntervalLabel.BackColor = System.Drawing.Color.Transparent;
+            this.textIntervalLabel.Location = new System.Drawing.Point(41, 205);
+            this.textIntervalLabel.Name = "textIntervalLabel";
+            this.textIntervalLabel.Size = new System.Drawing.Size(114, 15);
+            this.textIntervalLabel.TabIndex = 9;
+            this.textIntervalLabel.Text = "Text Update Interval:";
+            // 
+            // minimizeIcon
+            // 
+            this.minimizeIcon.ContextMenuStrip = this.minimizeIconContextMenu;
+            this.minimizeIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("minimizeIcon.Icon")));
+            this.minimizeIcon.Text = "OpenTimerResolution";
+            this.minimizeIcon.Visible = true;
+            this.minimizeIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.minimizeIcon_MouseClick);
+            // 
+            // minimizeIconContextMenu
+            // 
+            this.minimizeIconContextMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.quitToolStripMenuItem});
+            this.minimizeIconContextMenu.Name = "minimizeIconContextMenu";
+            this.minimizeIconContextMenu.Size = new System.Drawing.Size(98, 26);
+            this.minimizeIconContextMenu.Text = "OpenTimerResolution";
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(97, 22);
+            this.quitToolStripMenuItem.Text = "Quit";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitStripMenu_Click);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(327, 237);
+            this.Controls.Add(this.textIntervalLabel);
+            this.Controls.Add(this.intervalComboBox);
+            this.Controls.Add(this.warningLabel);
+            this.Controls.Add(this.timerResolutionBox);
+            this.Controls.Add(this.wantedResolutionLabel);
+            this.Controls.Add(this.maximumResolutionLabel);
+            this.Controls.Add(this.minimumResolutionLabel);
+            this.Controls.Add(this.currentResolutionLabel);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.startButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainWindow";
+            this.Text = "OpenTimerResolution";
+            this.Resize += new System.EventHandler(this.MainWindow_Resize);
+            this.minimizeIconContextMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button startButton;
+        private Button stopButton;
+        private Label currentResolutionLabel;
+        private Label minimumResolutionLabel;
+        private Label maximumResolutionLabel;
+        private System.Windows.Forms.Timer textChanger;
+        private Label wantedResolutionLabel;
+        private TextBox timerResolutionBox;
+        private Label warningLabel;
+        private ComboBox intervalComboBox;
+        private Label textIntervalLabel;
+        private NotifyIcon minimizeIcon;
+        private ContextMenuStrip minimizeIconContextMenu;
+        private ToolStripMenuItem quitToolStripMenuItem;
+    }
+}
---
+++ src/MainWindow.cs
@@ -0,0 +1,216 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace OpenTimerResolution
+{
+    public partial class MainWindow : Form
+    {
+        #region Definitions
+
+        /// <summary>
+        /// NtQueryTimerResolution returns the resolution of the system Timer in the context of the calling process.
+        /// </summary>
+        /// <param name="MinimumResolution">Minimum resolution defined by the system Timer.</param>
+        /// <param name="MaximumResolution">Maximum resolution defined by the system Timer.</param>
+        /// <param name="ActualResolution">Actual current resolution set.</param>
+        [DllImport("ntdll.dll", SetLastError = true)]
+        private static extern NtStatus NtQueryTimerResolution(out uint MinimumResolution, out uint MaximumResolution, out uint ActualResolution);
+
+        /// <summary>
+        /// NtSetTimerResolution sets the resolution of the system Timer in the calling process context.
+        /// </summary>
+        /// <param name="DesiredResolution">The desired resolution for the system Timer.</param>
+        /// <param name="SetResolution">Set to 'true' if setting a custom resolution, to 'false' if resetting to the default resolution.</param>
+        /// <param name="CurrentResolution">Current resolution set.</param>
+        [DllImport("ntdll.dll", SetLastError = true)]
+        private static extern NtStatus NtSetTimerResolution(int DesiredResolution, bool SetResolution, out int CurrentResolution);
+
+        private int NtCurrentResolution = 156250;
+        private uint NtMinimumResolution = 0;
+        private uint NtMaximumResolution = 0;
+        private uint NtActualResolution = 0;
+
+        #pragma warning disable CS8602 // Dereference of a possibly null reference.
+
+        private readonly static bool emptyBuildVersion = Assembly.GetEntryAssembly().GetName().Version.Build == -1;
+        private readonly string ProgramVersion = emptyBuildVersion ? Assembly.GetEntryAssembly().GetName().Version.Build.ToString() : "1.0.0.0";
+
+        #pragma warning restore CS8602 // Dereference of a possibly null reference.
+
+        /// <summary>
+        /// Receives the index of the Text Update Interval Combo Box, and returns the represented value.
+        /// </summary>
+        /// <param name="original">The index of the Text Update Interval Combo Box.</param>
+        /// <returns>Interval value for the timer.</returns>
+        private static int GetIntervalFromIndex(int original)
+        {
+
+            return original switch
+            {
+                0 => 1000,
+                1 => 500,
+                2 => 125,
+                3 => 50,
+                _ => 0,
+            };
+        }
+
+        #endregion
+
+
+
+
+        public MainWindow()
+        {
+            if (System.Diagnostics.Process.GetProcessesByName(System.Diagnostics.Process.GetCurrentProcess().ProcessName).Length > 1)
+            {
+                MessageBox.Show("OpenTimerResolution is already running. Only one instance of this application is allowed.", "OpenTimerResolution");
+                this.Close();
+                return;
+            }
+
+            InitializeComponent();
+
+            try
+            {
+                string[] args = Environment.GetCommandLineArgs();
+                for (int i = 1; i < args.Length; i++)
+                {
+                    string arg = args[i];
+
+                    switch (arg)
+                    {
+                        case "-minimized":
+                            {
+                                this.Hide();
+
+                                this.WindowState = FormWindowState.Minimized;
+
+                                timerResolutionBox.Text = "0.50";
+
+                                var result = NtSetTimerResolution(int.Parse(timerResolutionBox.Text) * 10000, true, out NtCurrentResolution);
+
+                                if (result != NtStatus.Success)
+                                    MessageBox.Show($"Error code: {result}", "OpenTimerResolution", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                                timerResolutionBox.ReadOnly = true;
+                                timerResolutionBox.Enabled = false;
+                                startButton.Enabled = false;
+                                stopButton.Enabled = true;
+                                break;
+                            }
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Wrong arguments, or something went wrong.");
+            }
+
+            this.Text = $"OpenTimerResolution | {ProgramVersion}";
+
+            intervalComboBox.SelectedIndex = 0; // Default is 1000ms.
+        }
+
+        private void timerResolutionBox_TextChanged(object sender, EventArgs e)
+        {
+            if (timerResolutionBox.Text != string.Empty)
+            {
+                warningLabel.Visible = (double.Parse(timerResolutionBox.Text) > 15.6250d) ? true : false;
+
+                //NtDesiredResolution = int.Parse((double.Parse(timerResolutionBox.Text) * 10000d).ToString());
+            }
+        }
+
+
+        private void timerResolutionBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            if ((e.KeyChar == '.') && (timerResolutionBox.Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+
+        private void intervalComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textChanger.Interval = GetIntervalFromIndex(intervalComboBox.SelectedIndex); // Changing the interval on index change.
+        }
+
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            var result = NtSetTimerResolution((int)(float.Parse(timerResolutionBox.Text) * 10000f), true, out NtCurrentResolution);
+
+            if (result != NtStatus.Success)
+            {
+                MessageBox.Show($"Error code: {result}", "OpenTimerResolution", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            timerResolutionBox.ReadOnly = true;
+            timerResolutionBox.Enabled = false;
+            startButton.Enabled = false;
+            stopButton.Enabled = true;
+        }
+
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            var result = NtSetTimerResolution((int)(float.Parse(timerResolutionBox.Text) * 10000f), false, out NtCurrentResolution);
+                
+            if (result != NtStatus.Success)
+            {
+                MessageBox.Show($"Error code: {result}", "OpenTimerResolution", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            timerResolutionBox.ReadOnly = false;
+            timerResolutionBox.Enabled = true;
+            startButton.Enabled = true;
+            stopButton.Enabled = false;
+        }
+
+
+        private void textChanger_Tick(object sender, EventArgs e)
+        {
+            NtQueryTimerResolution(out NtMinimumResolution, out NtMaximumResolution, out NtActualResolution);
+
+            // Values are represented in milliseconds.
+            currentResolutionLabel.Text = $"Current Resolution: {NtActualResolution * 0.0001:N4}ms";
+            minimumResolutionLabel.Text = $"Minimum Resolution: {NtMinimumResolution * 0.0001:N4}ms";
+            maximumResolutionLabel.Text = $"Maximum Resolution: {NtMaximumResolution * 0.0001:N4}ms";
+        }
+
+        private void MainWindow_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+                minimizeIcon.Visible = true;
+            }
+        }
+
+        private void minimizeIcon_MouseClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+            minimizeIcon.Visible = false;
+        }
+
+        private void quitStripMenu_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
