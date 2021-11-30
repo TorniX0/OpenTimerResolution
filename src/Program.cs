d197e917d24e55e2778ae0447996c5b5ebf61913
TorniX0 <43686655+TorniX0@users.noreply.github.com>
2021-11-30 12:05:02 +0200

    moved start args here
    to avoid a little issue, at startup it's not fully in tray, this should fix it.

--- src/Program.cs
+++ src/Program.cs
@@ -2,6 +2,7 @@ namespace OpenTimerResolution
 {
     internal static class Program
     {
+        private static string[] args = Environment.GetCommandLineArgs();
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
@@ -9,7 +10,29 @@ namespace OpenTimerResolution
         static void Main()
         {
             ApplicationConfiguration.Initialize();
-            Application.Run(new MainWindow());
+            MainWindow mainWind = new MainWindow();
+
+            try
+            {
+                for (int i = 1; i < args.Length; i++)
+                {
+                    string arg = args[i];
+
+                    switch (arg)
+                    {
+                        case "-minimized":
+                            mainWind.WindowState = FormWindowState.Minimized;
+                            mainWind.ShowInTaskbar = false;
+                            break;
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Wrong arguments, or something went wrong.");
+            }
+
+            Application.Run(mainWind);
         }
     }
 }
